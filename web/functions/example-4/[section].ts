3964a3b3d8b0a1070106cf76acb5c6e9bde29c61
Jason Cabot <jasoncabot@gmail.com>
2024-03-09 14:40:01 +0000

    Trigger redeploy for binding

--- web/functions/example-4/[section].ts
+++ web/functions/example-4/[section].ts
@@ -2,6 +2,10 @@ import { EventContext, Request } from "@cloudflare/workers-types";
 import { Env } from "..";
 
 export async function onRequest(context: EventContext<Env, string, Request>) {
+  if (context.env.SECTIONS === undefined) {
+    return new Response("SECTIONS binding is not set", { status: 500 });
+  }
+
   const sectionId = context.params.section as string;
 
   const validRegex = /^[a-z0-9\-_]{1,59}\.jabl$/;
